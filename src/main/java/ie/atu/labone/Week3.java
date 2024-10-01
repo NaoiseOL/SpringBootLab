00fccdf8a4ae73658222a7588d6aaebc357830c8
NaoiseOL <naoiseol@gmail.com>
2024-10-01 10:31:48 +0100

    New Project

---
+++ src/main/java/ie/atu/labone/Week3.java
@@ -0,0 +1,4 @@
+package ie.atu.labone;
+
+public class Week3 {
+}
---
+++ src/test/java/ie/atu/labone/LabOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
